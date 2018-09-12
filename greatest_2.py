fad22ae3dee0bb037ddec79a68347564facc422e
Jerome9886569178 <42990239+Jerome9886569178@users.noreply.github.com>
2018-09-12 10:34:09 +0530

    Add files via upload
    12/09/2018
    greatest of two numbers

---
+++ greatest_2.py
@@ -0,0 +1,9 @@
+#greatest of two variables
+a=int(input("Enter the value of 'a' "))
+b=int(input("Enter the value of 'b'"))
+if(a>b):
+	print(" 'a' is greater than 'b' ")
+else:
+	print(" 'b' is greater than 'a' ")
+	
+
