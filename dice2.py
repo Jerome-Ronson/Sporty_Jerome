44c1780d75971d48b795accba25ac34cf34578bb
Jerome9886569178 <42990239+Jerome9886569178@users.noreply.github.com>
2018-09-19 09:19:20 +0530

    Add files via upload

--- dice2.py
+++ dice2.py
@@ -1,14 +1,16 @@
 #input from  user to roll the dice
-a=str(input("enter r to roll the dice and q to quit"))
 i=0
 import random as ra
 while i<5:
+	a=str(input("enter r to roll the dice and q to quit"))
 	if(a=='r'):
 		print(ra.randint(1,6))
-		break
 	else:
-		if(a=='q'):
-			break
+		print("exit")
+		break
+		
+	
+		
 
 
 
