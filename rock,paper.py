79ee45e8345372e34a372d36150e9969229130d1
Jerome9886569178 <42990239+Jerome9886569178@users.noreply.github.com>
2018-10-05 10:31:07 +0530

    Add files via upload

--- rock,paper.py
+++ rock,paper.py
@@ -1,6 +1,8 @@
 #rock paper 
 import random 
 p={1:'r',2:'p',3:'s'}
+y=0
+c=0
 while True:
 	z=str(input("Enter a to play and b to quit "))
 	if(z=='a'):
@@ -11,23 +13,37 @@ while True:
 
 		if(yc=='r' and cc=='p'):
 			print("Computer Wins ")
+			c+=1
 		elif(yc=='r' and cc=='r'):
 			print("Draw ")
 		elif(yc=='r' and cc=='s'):
 			print("You win ")
+			y+=1
 		elif(yc=='p' and cc=='r'):
 			print("You win")
+			c+=1
 		elif(yc=='p' and cc=='p'):
 			print("Draw ")	
 		elif(yc=='p' and cc=='s'):
-			print("You win")
+			print("Computer wins")
+			y+=1
 		elif(yc=='s' and cc=='r'):
-			print("Computer wins")			
+			print("Computer wins")
+			c+=1			
 		elif(yc=='s' and cc=='p'):
 			print("You win ")
+			y+=1
 		elif(yc=='s' and cc=='s'):
 			print("Draw ")
 	else:
 		print("Bye ")
 		exit() 
-						
+	print("U won ",y)
+	print("Computer won ",c)
+	if(c==3 or y==3):
+		break
+								
+if(c>y):
+	print("Computer wins ")
+if(y>c):
+	print("you win ")	
